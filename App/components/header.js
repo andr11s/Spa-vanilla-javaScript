8c928b2c388db84ed8f3ecf00d1d36e749881af2
andr11s <alvisandres1@gmail.com>
2021-09-13 21:51:52 -0500

    refactor: menu reponsive

--- App/components/header.js
+++ App/components/header.js
@@ -7,18 +7,17 @@ export function Header() {
 			<div class="logo">
 				<h2 class="logo-item">STAR WARS</h2>
 			</div>
+			
 			<div class="menu-icons">
 				<i class="menu-bar fas fa-bars"></i>
 				<i class="menu-time fas fa-times-circle none"></i>
 			</div>
-		</div>
-		<nav class="nav none">
-			<ul class="menu">
-				<li class="menu-item"><a class="is-active" href="#/">Peliculas</a></li>
-				<li class="menu-item"><a href="#/people">Personajes</a></li>
-				<li class="menu-item"><a href="#/starships">Naves</a></li>
-			</ul>
-		</nav>
+			<nav class="menu"> 
+					<a class="is-active" href="#/">Peliculas</a>
+					<a href="#/people">Personajes</a>
+					<a href="#/starships">Naves</a> 
+			</nav>
+			</div>
 	`;
 
 	return $header;
--- App/helpers/menu.js
+++ App/helpers/menu.js
@@ -2,11 +2,20 @@ const d = document;
 
 d.addEventListener("click", (e) => {
 	$menuicons = d.querySelector(".menu-icons");
-	$nav = d.querySelector("nav");
+	$menu = d.querySelector(".menu");
 
 	if (e.target.matches(".menu-icons *")) {
+		let menuNone = $menu.classList.toggle("menu-isactive");
 		$menuicons.firstElementChild.classList.toggle("none");
 		$menuicons.lastElementChild.classList.toggle("none");
-		$nav.classList.toggle("none");
+
+		menuNone
+			? $menu.classList.add("menu-isactive")
+			: $menu.classList.remove("menu-isactive");
+	}
+
+	if (e.target.matches(".menu a")) {
+		if (!e.target.matches(".menu a")) return false;
+		$menu.classList.remove("menu-isactive");
 	}
 });
